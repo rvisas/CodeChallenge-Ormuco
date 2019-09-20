e5072aa8d68b75119084d441dfc66d97b44366fc
Ronny Visa <ronny.visa@gmail.com>
2019-09-20 00:12:57 -0500

    final version 1.01

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app = {
         const start1 = Math.min(x1, x2);
         const start2 = Math.min(x3, x4);
         // A dot cannot overlap another dot or another line... so we have to check that
-        if ((!length1) || (!length2)) return false;
+        if ((!length1) || (!length2))
+            return false;
+
         return (start1 < start2) ? ((start1 + length1) > start2) : ((start2 + length2) > start1);
     },
     // Returns -1 if string1 < string2;
@@ -39,8 +41,7 @@ const app = {
             }
         }
 
-        return result
-
+        return result;
     },
     validStrings: (s1, s2) => {
         const re = /^(\d{1,2})(\.\d{1,2})*$/;
